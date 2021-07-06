e58dfb27b9ac47e5c3f98f39df930424c6332d61
nguyenquocviet2007 <vietnguyen.170715@gmail.com>
2021-07-06 21:41:41 +0700

    remove redundant line

--- server/src/database/db_connection.js
+++ server/src/database/db_connection.js
@@ -4,11 +4,11 @@ const dbConfig = {
   host: "eu-cdbr-west-01.cleardb.com",
   password: "a9f0fef5",
   database: "heroku_d23ba00c4e094ee",
-  // multipleStatements: true
 };
 
 module.exports = () =>
   new Promise((resolve, reject) => {
+    
     const connection = mysql.createConnection(dbConfig);
 
     connection.connect((error) => {
