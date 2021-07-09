006e6834515c5e55847272c851c734ccf30c28c1
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2021-07-09 22:55:19 +0700

    fix episodes router's name, add authentication to get movie routers

--- server/src/routes/moviesRoutes.js
+++ server/src/routes/moviesRoutes.js
@@ -7,7 +7,7 @@ router.post("/postMovies", authtoken.Authentication,moviesHandler.postMovies);
 router.post("/postTvSeries", authtoken.Authentication,moviesHandler.postTvSeries);
 router.post("/postEpisodes", authtoken.Authentication,moviesHandler.postEpisodes);
 
-router.get("/getMovies", moviesHandler.getMovies);
-router.get("/getTvSeries", moviesHandler.getTvSeries);
-router.get("/getTvSeriesEpisode", moviesHandler.getEpisodes);
+router.get("/getMovies", authtoken.Authentication, moviesHandler.getMovies);
+router.get("/getTvSeries", authtoken.Authentication, moviesHandler.getTvSeries);
+router.get("/getEpisodes", authtoken.Authentication, moviesHandler.getEpisodes);
 module.exports = router;
