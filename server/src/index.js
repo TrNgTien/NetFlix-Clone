996720843f4653947cc88ecfb268e103ebc50ff4
Bill-Fashion <66458634+Bill-Fashion@users.noreply.github.com>
2021-07-16 14:48:31 +0700

    completed notification

--- server/src/index.js
+++ server/src/index.js
@@ -9,21 +9,14 @@ const io = require('socket.io')(http);
 const connectSocket = require('./notification/notifySocket');
 require("dotenv").config();
 
-app.get("/", (req, res) => res.sendFile(__dirname + '/index.html'));
-
 connectMongoDB();
 app.use(cors());
 app.use(express.json());
 routes(app);
 connectSocket(io);
-// routes(app);
 
 http.listen(PORT, () => {
   console.log(`Socket.IO server running at http://localhost:${PORT}/`);
 });
-// app.listen(PORT, () => {
-//   console.log(` Server is running in ${PORT}`);
-// });
-
 
 module.exports = app;
