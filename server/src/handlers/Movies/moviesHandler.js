95eed12b10de907a19f9ac287aa9761b76e5946b
nguyenquocviet2007 <vietnguyen.170715@gmail.com>
2021-08-02 18:14:07 +0700

    upload file to cloudinary and add genre

--- server/src/handlers/Movies/moviesHandler.js
+++ server/src/handlers/Movies/moviesHandler.js
@@ -1,15 +1,21 @@
 const sqlQuery = require("../../database/my_sql_query");
 const dbConnection = require("../../database/db_connection");
 const sendNotify = require("../Notification/notifyHandler");
+require("../../middlewares/multer/multer");
+require("dotenv").config();
+const cloudinary = require("../../database/cloudinary")
+
 
 module.exports = {
   postMovies: async (req, res) => {
-    const{name, duration, description, imdb_score, actor, director, year_release, moviesData} = req.body;
+    const{name, duration, description, imdb_score, actor, director, year_release,genreName, subgenre} = req.body;
     try {
       let connection = await dbConnection();
       let getMoviesNameQuery = `SELECT name FROM moviesinfor WHERE name =?`;
       let MoviesName = await sqlQuery(connection, getMoviesNameQuery, [name]);
       if (MoviesName.length === 0) {
+        let uploadedResponse = await cloudinary.uploader.upload(req.file.path, {resource_type: "video"});
+        let moviesData = uploadedResponse.secure_url;
         let postMoviesInforQuery = `INSERT INTO moviesinfor
                 (
                     name, duration, description, imdb_score, actor, director, year_release, moviesData
@@ -27,11 +33,26 @@ module.exports = {
         ]);
         let postMoviesQuery = `INSERT INTO movies(name) VALUE(?)`;
         let postMovies = await sqlQuery(connection, postMoviesQuery, [name]);
-        console.log(MoviesName);
+        let getmIDQuery = `SELECT mID FROM movies WHERE name =?`;
+        let getmID = await sqlQuery(connection, getmIDQuery, [name]);
+        let mID = getmID[0].mID;
+        let postGenreMovieQuery = `INSERT INTO genre(genreName, subgenre) VALUE(?,?)`;
+        let postGenreMovie = await sqlQuery(connection, postGenreMovieQuery, [genreName, subgenre]);
+        let getGenreIDQuery = `SELECT genreID FROM genre ORDER BY genreID DESC limit 1`;
+        let getGenreID = await sqlQuery(connection, getGenreIDQuery);
+        let genreID = getGenreID[0].genreID;
+        let insertMbelongtoGQuery = `INSERT INTO mbelongtog(genreID, mID) VALUE(?,?)`;
+        let insertMbelongtoG = await sqlQuery(connection, insertMbelongtoGQuery, [genreID, mID]);
+        //console.log(year_release);
+        //console.log(mID);
+        //console.log(genreID);
+        //console.log(getGenreID);
+        console.log("Movie", uploadedResponse);
         connection.end();
         sendNotify.notify(name, description);
         res.json({
           message: "Post successfully",
+          data: uploadedResponse
         });
       } else {
         connection.end();
@@ -50,7 +71,12 @@ module.exports = {
   getMovies: async (req, res) => {
     try {
       let connection = await dbConnection();
-      let getMoviesInforQuery = `SELECT * FROM moviesinfor`;
+      let getMoviesInforQuery = `SELECT 
+      moviesinfor.name, duration, description, imdb_score, actor, director, year_release, genreName, subgenre, moviesData
+      FROM moviesinfor, movies, mbelongtog, genre
+      WHERE movies.mID = mbelongtog.mID
+      AND genre.genreID = mbelongtog.genreID
+      AND movies.name = moviesinfor.name;`;
       let getMoviesInfor = await sqlQuery(connection, getMoviesInforQuery);
       connection.end();
       return res.status(200).json({ data: getMoviesInfor });
@@ -63,7 +89,7 @@ module.exports = {
   },
 
   postTvSeries: async (req, res) => {
-    const{name, description, imdb_score, actor, director, year_release, tvseriesData, season} = req.body;
+    const{name, description, imdb_score, actor, director, year_release, season, genreName, subgenre} = req.body;
     try {
       let connection = await dbConnection();
       let getTvSeriesNameQuery = `SELECT name FROM tvseriesinfor WHERE name =?`;
@@ -75,6 +101,8 @@ module.exports = {
         season,
       ]);
       if (TvSeriesName.length === 0 && TvSeriesSeason.length === 0) {
+        let uploadedResponse = await cloudinary.uploader.upload(req.file.path, {resource_type: "video"});
+        let tvseriesData = uploadedResponse.secure_url;
         let postTvSeriesInforQuery = `INSERT INTO TvSeriesinfor
             (
                 name, description, actor, director, year_release, tvseriesData
@@ -91,10 +119,22 @@ module.exports = {
           season,
           imdb_score,
         ]);
+        let postGenreTvSeiresQuery = `INSERT INTO genre(genreName, subgenre) VALUE(?,?)`;
+        let postGenreTvSeries = await sqlQuery(connection, postGenreTvSeiresQuery, [genreName, subgenre]);
+        let gettIDQuery = `SELECT tID FROM tvseries WHERE name =?`;
+        let gettID = await sqlQuery(connection, gettIDQuery, [name]);
+        let tID = gettID[0].tID;
+        let getGenreIDQuery = `SELECT genreID FROM genre WHERE genreName =?`;
+        let getGenreID = await sqlQuery(connection, getGenreIDQuery, [genreName]);
+        let genreID = getGenreID[0].genreID;
+        let insertTbelongtoGQuery = `INSERT INTO tbelongtog(genreID, tID) VALUE(?,?)`;
+        let insertTbelongtoG = await sqlQuery(connection, insertTbelongtoGQuery, [genreID, tID]);
+        console.log("TvSeries", uploadedResponse);
         sendNotify.notify(name, description);
         connection.end();
         res.json({
           message: "Post successfully",
+          data: uploadedResponse
         });
       } else {
         connection.end();
@@ -113,7 +153,12 @@ module.exports = {
   getTvSeries: async (req, res) => {
     try {
       let connection = await dbConnection();
-      let getTvSeriesInforQuery = `SELECT * FROM tvseriesinfor`;
+      let getTvSeriesInforQuery = `SELECT 
+      tvseriesinfor.name, description, imdb_score, actor, director, year_release, genreName, subgenre, tvseriesData
+      FROM tvseriesinfor, tvseries, tbelongtog, genre
+      WHERE tvseries.tID = tbelongtog.tID
+      AND genre.genreID = tbelongtog.genreID
+      AND tvseries.name = tvseriesinfor.name;`;
       let getTvSeriesInfor = await sqlQuery(connection, getTvSeriesInforQuery);
       connection.end();
       return res.status(200).json({ data: getTvSeriesInfor });
@@ -126,7 +171,7 @@ module.exports = {
   },
 
   postEpisodes: async (req, res) => {
-    const{name, duration, description, eName, episodesData} = req.body;
+    const{name, duration, description, eName} = req.body;
     try {
       let connection = await dbConnection();
       let getTIDQuery = `SELECT tID FROM tvseries WHERE name =?`;
@@ -137,6 +182,8 @@ module.exports = {
         eName,
       ]);
       if (getEpisodesName.length === 0) {
+        let uploadedResponse = await cloudinary.uploader.upload(req.file.path, {resource_type: "video"});
+        let episodesData = uploadedResponse.secure_url;
         let postEpisodesQuery = `INSERT INTO episodes(eName, description, duration, episodesData, tID) 
                                     VALUES (?,?,?,?,?)`;
         let postEpisodes = await sqlQuery(connection, postEpisodesQuery, [
@@ -166,7 +213,10 @@ module.exports = {
   getEpisodes: async (req, res) => {
     try {
       let connection = await dbConnection();
-      let getTvSeriesEpisodesQuery = `SELECT * FROM episodes`;
+      let getTvSeriesEpisodesQuery = `SELECT 
+      eName, duration, description, episodesData, tvseries.name, tvseries.season
+      FROM episodes, tvseries
+      WHERE tvseries.tID = episodes.tID;`;
       let getTvSeriesEpisodes = await sqlQuery(
         connection,
         getTvSeriesEpisodesQuery
--- server/src/routes/moviesRoutes.js
+++ server/src/routes/moviesRoutes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const moviesHandler = require("../handlers/Movies/moviesHandler");
 const authtoken = require("../middlewares/authentication/authentication");
+const upload = require("../middlewares/multer/multer");
 
-router.post("/postMovies", authtoken.Authentication, authtoken.adminVerify, moviesHandler.postMovies);
-router.post("/postTvSeries", authtoken.Authentication, authtoken.adminVerify, moviesHandler.postTvSeries);
-router.post("/postEpisodes", authtoken.Authentication, authtoken.adminVerify, moviesHandler.postEpisodes);
+
+router.post("/postMovies", upload.single('video'), authtoken.Authentication, authtoken.adminVerify, moviesHandler.postMovies);
+router.post("/postTvSeries", upload.single('video'), authtoken.Authentication, authtoken.adminVerify, moviesHandler.postTvSeries);
+router.post("/postEpisodes", upload.single('video'), authtoken.Authentication, authtoken.adminVerify, moviesHandler.postEpisodes);
 
 router.get("/getMovies", authtoken.Authentication, moviesHandler.getMovies);
 router.get("/getTvSeries", authtoken.Authentication, moviesHandler.getTvSeries);
